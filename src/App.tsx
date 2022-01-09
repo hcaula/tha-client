f6674c6ce6d1ab85e08fec4273b4db23ba2b98ca
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-09 13:57:52 -0300

    chore: download and configure assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react'
-import logo from './logo.svg'
+import logo from 'assets/icons/origin-logo.svg'
+import compactLogo from 'assets/icons/origin-logo-compact.svg'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello!
+      <img src={logo} />
+      <img src={compactLogo} />
     </div>
   )
 }
