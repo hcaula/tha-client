dd57ce04990dc2c8f76627ed277fb18cc4dfea05
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-09 16:29:09 -0300

    feat(Footer): implement Footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,5 +1,21 @@
+import { useTranslation } from 'react-i18next'
+
+import lockIcon from 'assets/icons/lock-icon.svg'
 import styles from './Footer.module.css'
 
-const Footer = () => <div className={styles.Footer}>Hi, I'm Footer</div>
+const Footer = () => {
+  const { t } = useTranslation('common')
+
+  return (
+    <div className={styles.Footer}>
+      <div className={styles.content}>
+        <img src={lockIcon} alt="" className={styles.lockIcon} />
+        <div className={styles.disclaimer}>
+          {t('Footer.encryptionDisclaimer')}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Footer
