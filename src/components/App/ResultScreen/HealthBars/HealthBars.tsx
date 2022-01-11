949d485a59a4e00ff31d1b274a9f0579e1c0326f
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-10 22:11:19 -0300

    feat(HealthBars: implement health bars

---
+++ src/components/App/ResultScreen/HealthBars/HealthBars.tsx
@@ -0,0 +1,30 @@
+import styles from './HealthBars.module.css'
+
+export type HealthBarsColor = 'healthy' | 'medium' | 'low'
+
+type HealthBarsProps = {
+  /* TODO: a CSS only approach makes it hard to know the index of the child. Therefore,
+   * this component only supports 3 sub bars. Change to a JS approach. */
+  levels: 3
+  coloredLevels: number
+  color: HealthBarsColor
+}
+
+const HealthBars = ({ levels, coloredLevels, color }: HealthBarsProps) => {
+  const range = Array.from(Array(levels).keys())
+
+  return (
+    <div className={styles.HealthBars}>
+      {range.map((_, index) => {
+        const isColored = index < coloredLevels
+        return (
+          <div
+            className={`${styles.healthBar} ${isColored && styles[color]}`}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default HealthBars
---
+++ src/components/App/ResultScreen/HealthBars/index.tsx
@@ -0,0 +1,4 @@
+import { HealthBarsColor as HealthBarsColorType } from './HealthBars'
+
+export type HealthBarColor = HealthBarsColorType
+export { default } from './HealthBars'
