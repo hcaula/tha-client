6be118b7cdb439c7cc22d3fe93a17b8f7da4fe57
Henrique Caúla <henrique.caula3@gmail.com>
2022-01-10 22:58:12 -0300

    feat(Loading): add loading component

--- src/components/App/ResultScreen/Loading/Loading.tsx
+++ src/components/App/ResultScreen/Loading/Loading.tsx
@@ -1,5 +1,9 @@
 import styles from './Loading.module.css'
 
-const Loading = () => <div className={styles.Loading}>Loading...</div>
+const Loading = () => (
+  <div className={styles.Loading}>
+    <div className={styles.loader} />
+  </div>
+)
 
 export default Loading
